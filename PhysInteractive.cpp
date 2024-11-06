c5033e8311c97a56ab8d419cc39bf1ed5237f08e
J Miller <jwmiller006@outlook.com>
2024-11-06 09:34:29 -0500

    Initial Commit

---
+++ PhysInteractive.cpp
@@ -0,0 +1,12 @@
+﻿// PhysInteractive.cpp : Defines the entry point for the application.
+//
+
+#include "PhysInteractive.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ PhysInteractive.h
@@ -0,0 +1,8 @@
+﻿// PhysInteractive.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
